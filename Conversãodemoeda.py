e09b5e1c41e6d7dd0861437ee58ae9d2ee455dd4
Adryelle Sanae <drica_sanae@hotmail.com>
2022-01-06 15:37:25 -0400

    Script para conversão de moeda

---
+++ Conversãodemoeda.py
@@ -0,0 +1,11 @@
+#Faça um programa que peça um valor em dólares e converta pra um valor em reais e mostre na tela  o resultado da conversão.
+#FORMULA: 1 dólar = 5.68 reais'''
+
+#Entrada
+dolar = float(input("Digite o valor em dólares: "))
+
+#Processamento
+real = dolar*5.68
+
+#Saída
+print(dolar, "doláres correspondem a ", real, "reais.")
