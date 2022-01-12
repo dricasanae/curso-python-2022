66c8766ffe2680c42ade370bcbc90b7871bd8689
Adryelle Sanae <drica_sanae@hotmail.com>
2022-01-12 18:38:09 -0400

    exxercicio aula while

---
+++ exercicio3while.py
@@ -0,0 +1,14 @@
+#Faça um programa que, dado uma lista de N números, 
+# determine o menor valor, o maior valor e a soma dos valores.
+n = int(inpu("Insira quantos numero quer colocar: "))
+lista = []
+for indice in range (n):
+    numero = float(input("Digite  número: "))
+    listanumero = 
+lista = [12, 1, 24, 13, 86, 42, 89]
+lista.sort()
+print("Lista:", lista)
+print("O menor numero:", lista[0])
+print("O maior numero:", lista[-1])
+soma = lista[0] + lista[-1]
+print( lista[0],"+", lista[-1],"=",soma)
