2388e637d6b8887c1f59293e50f25c9bff155e66
Adryelle Sanae <drica_sanae@hotmail.com>
2022-01-13 13:13:12 -0400

    exercicio de aula sobre modulos

---
+++ geometria/areas.py
@@ -0,0 +1,9 @@
+#função area do circulo
+def area_circulo(raio):
+    areac = 3.14*(raio**2)
+    return areac
+
+#função area do quadrado
+def area_quadrado(lado):
+    areaq = lado**2
+    return areaq
